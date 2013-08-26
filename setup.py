1782d09feb01c2465f7f398100abab63259b0d5d
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2013-08-26 12:45:08 +0200

    Update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 setup(
 
     name='xbob.db.verification.filelist',
-    version='1.3.1',
+    version='1.3.2a0',
     description='Verification File List Database Access API for Bob',
     url='https://pypi.python.org/pypi/xbob.db.verification.filelist',
     license='GPLv3',
